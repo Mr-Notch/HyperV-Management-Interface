7ba357050f6e63bd274556c7832a24abc1caa7f0
Sam_Chai <80421117@qq.com>
2021-02-16 22:00:21 +0800

    Add SMTP Config Setter

--- Injector.py
+++ Injector.py
@@ -19,9 +19,20 @@ template_location = "D:\\HyperV-Management-Interface-main\\VirtualMachine\\Templ
 maturity_keep_time = "7"
 # 虚拟机超过到期保留时间后是否删除（若否则存放在虚拟机路径的recovery文件夹内）
 maturity_delete_switch = True
+# 邮件SMTP服务器
+smtp_server = "smtp.163.com"
+# 发送者用户名
+smtp_sender = "HydrogenCloud@163.com"
+# 邮件登录用户名
+smtp_login_user = "HydrogenCloud@163.com"
+# 邮件SMTP服务器密码
+smtp_server_password = "LECOGDYYBJUJJBST"
+# 邮件接收人
+smtp_receiver_user = ["Mr_Notch@163.com","2222002611@qq.com","2628869101@qq.com"]
 
 
-
+# 以下为适配2.0版本做出的函数调用
+# 邮件服务器仍使用3.0（调用变量）
 
 def getSysLocation():
     sysloc = sys_location
@@ -50,12 +61,12 @@ def getMaturityDeleteSwitch():
 
 main_config = {
     "hmi.system.location": sys_location,
-    "hmi.system.func.location": sys_location + '\\func',
-    "hmi.system.utilities.location": sys_location + '\\utilities',
-    "hmi.system.config.location": sys_location + '\\config',
-    "hmi.vm.location": sys_location + '\_Files\VTs',
-    "hmi.iso.location": sys_location + '\_Files\iso',
-    "hmi.template.location": sys_location + '\_Files\\templates',
+    "hmi.system.func.location": sys_location + 'func',
+    "hmi.system.utilities.location": sys_location + 'utilities',
+    "hmi.system.config.location": sys_location + 'config',
+    "hmi.vm.location": sys_location + '_Files\\VTs',
+    "hmi.iso.location": sys_location + '_Files\\iso',
+    "hmi.template.location": sys_location + '_Filestemplates',
     "hmi.system.interface": "PowerShell"
 }
 
