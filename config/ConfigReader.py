3124adbf4ed80a6c9b54c8dadc27426a16a4d9e7
Sam_Chai <80421117@qq.com>
2021-02-16 21:59:13 +0800

    Remove ConfigReader Functions

--- config/ConfigReader.py
+++ config/ConfigReader.py
@@ -1,10 +1,12 @@
 # -- coding: utf-8 --
-import json
-import Injector
+# import json
+# import Injector
+#
+# sys_path=Injector.getSysLocation()
+#
+# with open(sys_path+'config/config.json','r',encoding='utf8')as fp:
+#     config_data = json.load(fp)
+#     print('这是文件中的json数据：',config_data)
+#     print('这是读取到文件数据的数据类型：', type(config_data))
 
-sys_path=Injector.getSysLocation()
-
-with open(sys_path+'config/config.json','r',encoding='utf8')as fp:
-    config_data = json.load(fp)
-    print('这是文件中的json数据：',config_data)
-    print('这是读取到文件数据的数据类型：', type(config_data))
+# daemon\checkMaturityTime 包现在已取代该功能
